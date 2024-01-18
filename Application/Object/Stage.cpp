88cc6bb89c2f3e2bafafcd5b71731bdc75a08d6f
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2024-01-18 11:52:17 +0900

    ステージのモデルを仮追加

--- Application/Object/Stage.cpp
+++ Application/Object/Stage.cpp
@@ -3,73 +3,40 @@
 
 Stage::Stage()
 {
-	GameObject::Initialize("player");
+	GameObject::Initialize("Stage");
 }
 
 void Stage::Initialize()
 {
 	world_.Initialize();
 	world_.translate_.y = 1.0f;
-	world_.scale_.x = radius_ * kTransformRadius_;
-	world_.scale_.z = radius_ * kTransformRadius_;
-	mode_ = None;
+	world_.scale_.x = kMaxRadius_ * kTransformRadius_;
+	world_.scale_.z = kMaxRadius_ * kTransformRadius_;
+	//mode_ = None;
 	world_.UpdateMatrix();
+
+
 }
 
 void Stage::Update()
 {
-	switch (mode_)
-	{
-	case Stage::None:
-		break;
-	case Stage::Shrink:
-		if (5.0f < radius_)
-		{
-			radius_ -= 0.03f;
-		}
-		else
-		{
-			radius_ = kMaxRadius_ * 0.5f;
-		}
-		break;
-	case Stage::Expand:
-		radius_ += 2.5f;
-		if (kMaxRadius_ < radius_)
-		{
-			radius_ = kMaxRadius_;
-		}
-		mode_ = Shrink;
-		break;
-	default:
-		break;
-	}
-	world_.scale_.x = radius_ * kTransformRadius_;
-	world_.scale_.z = radius_ * kTransformRadius_;
 	world_.UpdateMatrix();
 }
 
 void Stage::DebagWindow()
 {
-	model_->DebugParameter("player");
-#ifdef _DEBUG
+	model_->DebugParameter("Stage");
 
 	ImGui::Begin("Stage");
 
-	if (ImGui::RadioButton("None", mode_ == None))
-		mode_ = None;
-	if (ImGui::RadioButton("Shrink", mode_ == Shrink))
-		mode_ = Shrink;
-	if (ImGui::RadioButton("Expand", mode_ == Expand))
-		mode_ = Expand;
+	ImGui::DragFloat("maxRadius", &kMaxRadius_, 0.1f);
 
-	if (ImGui::GetIO().KeysDown[ImGuiKey_Space])
-	{
-		mode_ = Expand;
-	}
+	ImGui::DragFloat("scale", &kTransformRadius_, 0.01f);
 
 	ImGui::End();
 
-#endif // _DEBUG
+	world_.scale_.x = kMaxRadius_ * kTransformRadius_;
+	world_.scale_.z = kMaxRadius_ * kTransformRadius_;
 
 }
 
--- Application/Object/Stage.h
+++ Application/Object/Stage.h
@@ -1,27 +1,18 @@
 #pragma once
 
 #include "SingleGameObjects/GameObject.h"
+
+
 class Stage : public GameObject
 {
 private:
-	enum ActionMode
-	{
-		None,   // 変更なし
-		Shrink, // 縮める
-		Expand, // 拡げる
-	};
 
 private:
 	// ステージの最大値
 	float kMaxRadius_ = 20.0f;
 
-	// ステージの半径
-	float radius_ = kMaxRadius_;
-
 	// ステージに適用するスケールの倍率
 	float kTransformRadius_ = 1.0f;
-
-	ActionMode mode_ = None;
 
 public:
 	Stage();
