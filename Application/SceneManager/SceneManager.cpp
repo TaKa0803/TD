2a4c92f3e189bb373fea871fd743dd5f8b0e341b
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2024-01-23 15:50:12 +0900

    シーンマネージャーにデバッグ用の文字を追加

--- Application/SceneManager/SceneManager.cpp
+++ Application/SceneManager/SceneManager.cpp
@@ -19,8 +19,6 @@
 void SceneManager::Initialize()
 {
 	currentSceneNo_ = 0;
-	//初期シーン設定
-	sceneArr_[0]->SetSceneNo(TITLE);
 
 	//シーンの数取得
 	sceneArr_.resize((size_t)SCENE::SceneCount);
@@ -34,7 +32,12 @@ void SceneManager::Initialize()
 	sceneName_.clear();
 	sceneName_.push_back("TITLE");
 	sceneName_.push_back("GAME");
+	sceneName_.push_back("CLEAR");
+	sceneName_.push_back("FAIL");
 	sceneName_.push_back("DEBUG");
+
+	//初期シーン設定
+	sceneArr_[0]->SetSceneNo(GAME);
 	/*
 	sceneArr_[TITLE] = std::make_unique<TitleScene>();
 	sceneArr_[STAGE] = std::make_unique<PlayScene>();
